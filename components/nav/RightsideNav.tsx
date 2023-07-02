e0a0b94c8185514b27e25b84ff555bad84ccfc61
jaxonhill <jaxoncharleshill@gmail.com>
2023-07-02 00:06:58 -0700

    Add social links to desktop nav bar with group hover effects

--- components/nav/RightsideNav.tsx
+++ components/nav/RightsideNav.tsx
@@ -30,6 +30,22 @@ export default function RightsideNav({ links, socials }: RightsideNavProps) {
 					</Link>
 				);
 			})}
+			{socials.map((social) => {
+				return (
+					<a
+						href={social.href}
+						target="_blank"
+						className="group w-full h-full flex gap-2 items-center"
+					>
+						<p className="w-full h-full text-slate-500 group-hover:text-slate-100">
+							{social.platform}
+						</p>
+						<i className="w-4 h-4 fill-slate-500 group-hover:fill-slate-100">
+							{social.svgElement}
+						</i>
+					</a>
+				);
+			})}
 		</div>
 	);
 }
