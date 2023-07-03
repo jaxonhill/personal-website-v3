6752ac7a75433004058bb31bd28636b938ae9bdd
jaxonhill <jaxoncharleshill@gmail.com>
2023-07-02 22:55:46 -0700

    Change project type function to fetch in same function and throw error

--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
 ### Navbar
+- [x] Add menu dropdown for mobile
+- [x] Add responsive breakpoint to have normal links
 - [x] Fix highlighting for main navbar
 - [x] Make logo go back to home also
 - [x] Add "socials" array of social objects with their logos
@@ -11,24 +13,16 @@
 - [x] Add main header
 - [x] Add secondary text
 - [x] Add main button to go to my projects
-- [ ] Add gradient?
+- [ ] Add breakpoints for responsiveness
 
 ### Projects
-- [ ] Create some sort of CMS with markdown and JSON
+- [x] Create some sort of CMS with markdown and JSON
 - [ ] Add loading state for the content, make artifial promise to test this
-- [ ] Add error state for the content, make artificial error to test this
+- [x] Add error state for the content, make artificial error to test this
 - [ ] Add heading
 - [ ] Add search bar
-- [ ] Add filters drop down
+- [ ] Add technologies drop down
 - [ ] Add project grid
-- [ ] Add project card with title, description, see more that expands into individual page
-- [ ] Add technologies drop down on each card
-
-### Individual Project Page
-- [ ] Add individual page based on slug
-- [ ] Add image at the top with slight gradient fading into page
-- [ ] Add technology badges under the image, truncate them if longer than the length of the page and have a dropdown where you can see all of them
-- [ ] Add title, full description
-
-### Cool feature, but is it useful?
-- [ ] I think you can extra URL params on a route and get them in the base component of that page so you could theoretically have each badge of a technology be a link to filter the projects by a certain technology
+- [ ] Add project card with title, image, description
+- [ ] Add technologies hover thing
+- [ ] Add responsive breakpoints to instead have sidebar with searchbar and technologies dropdown
--- lib/notion.ts
+++ lib/notion.ts
@@ -8,7 +8,60 @@ type NotionResultsObject = {
     properties: any;
 }
 
+type ProjectFromNotion = {
+	status: {
+		id: string;
+		type: string;
+		status: object;
+	};
+	github_url: {
+		id: string;
+		type: string;
+		url: null;
+	};
+	created_at: {
+		id: string;
+		type: string;
+		created_time: string;
+	};
+	description: {
+		id: string;
+		type: string;
+		rich_text: Array<any>;
+	};
+	live_site_url: {
+		id: string;
+		type: string;
+		url: null;
+	};
+	technologies: {
+		id: string;
+		type: string;
+		multi_select: Array<any>;
+	};
+	image: {
+		id: string;
+		type: string;
+		files: Array<any>;
+	};
+	title: {
+		id: string;
+		type: string;
+		title: Array<any>;
+	};
+};
+
+type Project = {
+	title: string;
+	description: string;
+	imageURL: string;
+	technologies: string[];
+	github_url: string | null;
+	live_site_url: string | null;
+};
+
 export async function getAllProjects() {
+    let results: NotionResultsObject[];
     try {
     // Connect to Notion
     const notion = new Client({
@@ -34,16 +87,57 @@ export async function getAllProjects() {
     }) as NotionResponse;
 
     // Get the results from the response
-    const results: NotionResultsObject[] = response.results;
-
-    // Map over the results and only get the information needed
-    return results.map((result) => {
-        return result.properties;
-    });
+    results = response.results;
 
     // Catch any errors if not connected to Notion
     } catch (error: unknown) {
         throw new Error("Something went wrong with Notion.");
     }
+
+    try {
+    // Map over the results and only get the information needed
+    const projectsFromNotion: ProjectFromNotion[] = results.map((result) => {
+        return result.properties;
+    });
+
+    // Return projects in correct format
+    return projectsFromNotion.map((notionProject: ProjectFromNotion) => {
+        return extractInformationIntoType(notionProject);
+    });
+    } catch (error: unknown) {
+        throw new Error("One of the projects does not meet the schema.");
+    }
 }
 
+function extractInformationIntoType(
+	projFromNotion: ProjectFromNotion
+): Project {
+	// Title
+	const title: string = projFromNotion.title.title[0].plain_text;
+	// Description
+	const description: string =
+		projFromNotion.description.rich_text[0].plain_text;
+	// Image URL
+	const imageURL: string = projFromNotion.image.files[0].file.url;
+	// Technologies
+	const technologies: string[] = projFromNotion.technologies.multi_select.map(
+		(tech) => {
+			return tech.name;
+		}
+	);
+	// Github URL
+	let github_url: string | null = projFromNotion.github_url.url;
+	github_url = projFromNotion.github_url.url;
+	// Live Site URL
+	let live_site_url: string | null = projFromNotion.live_site_url.url;
+
+	// Return formatted project
+	return {
+		title,
+		description,
+		imageURL,
+		technologies,
+		github_url,
+		live_site_url,
+	};
+}
