647fb99f0f4922361d1f229471e8ee8708428c5b
jaxonhill <jaxoncharleshill@gmail.com>
2023-07-06 21:25:22 -0700

    Add github button

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -74,14 +74,19 @@ export default async function Projects() {
 					return (
 						<div className="border-2 border-slate-800 p-6 rounded-2xl">
 							<div className="w-full aspect-video relative">
-								{/* <Image
+								<Image
 									src={project.imageURL}
 									alt={project.title}
 									fill
 									className="object-cover overflow-hidden rounded-lg"
-								/> */}
+								/>
 							</div>
-							<div className="flex w-full gap-2">
+							<p
+								className={`${poppins.className} text-2xl font-bold text-slate-100 pt-4 pb-2`}
+							>
+								{project.title}
+							</p>
+							<div className="flex w-full gap-2 pb-3">
 								{project.technologies.map((technology) => {
 									return (
 										<div className="w-6 h-6 aspect-square relative">
@@ -101,14 +106,41 @@ export default async function Projects() {
 									);
 								})}
 							</div>
-							<p
-								className={`${poppins.className} text-2xl font-bold text-slate-100 pt-4 pb-2`}
-							>
-								{project.title}
-							</p>
-							<p className="text-slate-400">
+							<p className="text-slate-400 pb-4">
 								{project.description}
 							</p>
+							<div className="w-full flex justify-end">
+								{!project.github_url ? (
+									<a
+										href={""}
+										className="flex items-center gap-2 rounded-lg font-medium py-2 px-4 w-fit bg-slate-100 text-slate-950 hover:bg-slate-100/90"
+									>
+										<p className="text-slate-950">Github</p>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="16"
+											height="16"
+											viewBox="0 0 24 24"
+											className="fill-slate-950"
+										>
+											<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+										</svg>
+									</a>
+								) : (
+									<div className="flex items-center gap-2 rounded-lg font-medium py-2 px-4 w-fit opacity-30 bg-slate-100 text-slate-950 cursor-not-allowed">
+										<p className="text-slate-950">Github</p>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="16"
+											height="16"
+											viewBox="0 0 24 24"
+											className="fill-slate-950"
+										>
+											<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+										</svg>
+									</div>
+								)}
+							</div>
 						</div>
 					);
 				})}
