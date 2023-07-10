89e32f6789f3908aee4476893e6e9a26826c49a9
jaxonhill <jaxoncharleshill@gmail.com>
2023-07-10 12:58:25 -0700

    Add section for all link dropdowns to make DropdownNav less crowded

---
+++ components/nav/LinksSectionDropdown.tsx
@@ -0,0 +1,23 @@
+import { NavElement } from "@/types";
+import LinkButtonDropdown from "./LinkButtonDropdown";
+import { usePathname } from "next/navigation";
+
+interface LinksSectionDropdownProps {
+	links: NavElement[];
+}
+
+export default function LinksSectionDropdown({
+	links,
+}: LinksSectionDropdownProps) {
+	// Get pathname to check which nav link is active page in map function
+	const pathname = usePathname();
+
+	return (
+		<div>
+			{links.map((link) => {
+				const isActive = pathname.endsWith(link.href);
+				return <LinkButtonDropdown isActive={isActive} link={link} />;
+			})}
+		</div>
+	);
+}
